b31612e54cead97666fdace09fb14ce32a488d66
이재웅 <ijw9209@naver.com>
2022-11-07 18:24:08 +0900

    query-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, {useState} from 'react';
+import { useGetPokemonByNameQuery } from './service/pokemon';
+
+const pokemon = ['bulbasaur', 'pikachu', 'ditto', 'bulbasaur'];
 
 function App() {
+
+  const [pollingInterval, setPollingInterval] = useState(0)
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <select
+      onChange={(change) => setPollingInterval(Number(change.target.value))}
+    >
+      <option value={0}>Off</option>
+      <option value={1000}>1s</option>
+      <option value={5000}>5s</option>
+    </select>
+    <div>
+      {pokemon.map((poke, index) => (
+        <Pokemon key={index} name={poke} pollingInterval={pollingInterval} />
+      ))}
     </div>
+  </div>
   );
 }
 
 export default App;
+
+
+
+
+export const Pokemon = ({
+  name,
+  pollingInterval,
+}: {
+  name: string
+  pollingInterval: number
+}) => {
+  const { data, error, isLoading, isFetching } = useGetPokemonByNameQuery(
+    name,
+    {
+      pollingInterval,
+    }
+  )
+
+  return (
+    <>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>
+            {data.species.name} {isFetching ? '...' : ''}
+          </h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </>
+  )
+}
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,22 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { counterSlice } from '../features/counter/counterSlice';
+// Or from '@reduxjs/toolkit/query/react'
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { pokemonApi } from '../service/pokemon';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter : counterSlice.reducer,
+    // Add the generated reducer as a specific top-level slice
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
